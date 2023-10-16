8232dd43708a3980d41646b2bf89b136a3724ee8
Maheswaran Palaniselvan <maheswaran.palaniselvan@gmail.com>
2023-10-16 21:03:52 +0530

    working copy of main_pseudo

--- kite_monitor/main_pseudo.py
+++ kite_monitor/main_pseudo.py
@@ -1,9 +1,12 @@
-from pandas import DataFrame
+import pandas as pd
 import utils
 import utils_playwright
 from playwright.sync_api import sync_playwright
 import _kite_connect
 import sys
+
+kite_chart_url = 'https://kite.zerodha.com/chart/web/ciq/NFO-OPT/{option_name}/{instrument_token}?theme=dark'
+
 
 def underlying_from_config(symbol_details_from_config: list[dict]) -> list:
     """
@@ -23,20 +26,49 @@ def ltp_for_underlying(lst_underlying: list, kite_client: _kite_connect.KiteApp)
     """
     output_dict = {}
     for underlying in lst_underlying:
-        key = underlying.split(":")[-1].split()[0]
+        # key = underlying.split(":")[-1].split()[0]
         tmp = kite_client.ltp(underlying).get(underlying)
         if tmp:
-            output_dict[key] = tmp.get("last_price")
+            output_dict[underlying] = tmp.get("last_price")
     print(output_dict)
     return output_dict
 
+def get_instrument_token(option_name, df):
+    tokens = df[df['tradingsymbol'] ==
+                option_name]['instrument_token'].to_list()
+    return tokens[0] if len(tokens) >= 1 else 0
 
-def atm_from_ltp(dct_underlying: dict) -> dict:
-    """
-       returns dict of form 
-        {"BANKNIFTY": 43000, "NSE:NIFTY 50": 12000}               
-    """
-    pass
+
+def coin_option_names(symbol_details_from_config, instrument_details, dct_ltp):
+    # delete first element common
+    for symbol, details in symbol_details_from_config.items():
+        print(details)
+        # symbol = list(details.keys())[0]  # Get the symbol from the dictionary
+        # if symbol != "common":
+        # details = details[symbol]  # Extract details for the symbol
+        ltp = dct_ltp[details['underlying']]
+        atm = utils.get_atm(details['diff'], ltp)
+
+        val = atm + details['ce']
+        option_name = symbol + details['expiry'] + str(val) + 'CE'
+        instrument_token = get_instrument_token(
+            option_name, instrument_details)
+
+        details["ce_" + "url"] = kite_chart_url.format(
+            option_name=option_name, instrument_token=instrument_token)
+        details['call'] = option_name
+
+        val = atm + details['pe']
+        option_name = symbol + details['expiry'] + str(val) + 'PE'
+        instrument_token = get_instrument_token(
+            option_name, instrument_details)
+
+        details["pe_" + "url"] = kite_chart_url.format(
+            option_name=option_name, instrument_token=instrument_token)
+        details['put'] = option_name
+
+    return symbol_details_from_config
+
 
 
 def options_from_ltp(dct_atm: dict) -> dict:
@@ -50,20 +82,41 @@ def options_from_ltp(dct_atm: dict) -> dict:
     pass
 
 
-def download_playwright(dct_option: dict) -> DataFrame:
+def download_playwright(url: str, context) -> pd.DataFrame:
     """
         if the option name is supplied this returns the 
         dataframe. we want to reuse this later for exits
     """
-    pass
+    page3 = context.new_page()
+    page3.goto(url)
+    page3.wait_for_load_state("networkidle")
+    page3.frame_locator("#chart-iframe").locator(".ciq-DT > span").first.click()
+    page3.wait_for_load_state("networkidle")
+    with page3.expect_download() as download_info:
+        page3.frame_locator("#chart-iframe").get_by_role("button", name="Download").click()
+        download1 = download_info.value
+        df = pd.read_csv(f"{download1.path()}")
+    page3.close()
+    return df
 
 
-def generate_signal_fm_df(df_ce: DataFrame, df_pe: DataFrame) -> str:
+def generate_signal_fm_df(df_ce: pd.DataFrame, df_pe: pd.DataFrame) -> str:
     """
        if signal is generated it return the name of the option
        a dictionary can be built from the caller of this function.
        {"BANKNIFTY": {"signal": "BANKNIFTY23OCT42800CE"}}
     """
+    print(df_ce.head())
+    print(df_pe.head())
+    print(df_ce.columns)
+    neg_3_ce_value = df_ce['Close'].iloc[-3]
+    neg_2_ce_value = df_ce['Close'].iloc[-2]
+    neg_3_pe_value = df_pe['Close'].iloc[-3]
+    neg_2_pe_value = df_pe['Close'].iloc[-2]
+    if neg_3_ce_value < neg_3_pe_value and neg_2_ce_value > neg_2_pe_value:
+        print(f"CE>PE(Crossing) is achieved")
+    elif neg_3_pe_value < neg_3_ce_value and neg_2_pe_value > neg_2_ce_value:
+        print(f"PE>CE(Crossing) is achieved")
     pass
 
 
@@ -112,16 +165,23 @@ kite_client = _kite_connect.KiteApp(enc_token)
 
 symbol_details_with_common: list[dict] = configuration_details["symbols"]
 symbol_details: dict = utils.merge_common_to_symbols(symbol_details_with_common)
+print(symbol_details)
 dct_underlying: list = underlying_from_config(symbol_details)
 print(dct_underlying)
 position = False
-
+instrument_details = utils.get_instrument_details()
 while True:
     if not position:
         dct_ltp = ltp_for_underlying(dct_underlying, kite_client)
+        updated_configuration_details = coin_option_names(symbol_details, instrument_details, dct_ltp)
+        print(updated_configuration_details)
+        for symbol, details in updated_configuration_details.items():
+            df_ce = download_playwright(details["ce_url"], context)
+            df_pe = download_playwright(details["pe_url"], context)
+            str_symbol = generate_signal_fm_df(df_ce, df_pe)
         break
 #         # need to get it through kiteweb, no redis
-#         dct_atm = atm_from_ltp(dct_ltp)
+        dct_atm = atm_from_ltp(dct_ltp)
 #         # get the option symbols involved
 #         dct_options = options_from_ltp(dct_atm)
 #         # TODO: this is mostly incorrect, adjust accordingly
