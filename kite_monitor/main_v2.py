6cbc069695dd0ddacdd5304c65dc3299dabdc988
Maheswaran Palaniselvan <maheswaran.palaniselvan@gmail.com>
2024-04-10 16:47:28 +0530

    fix for ]

--- kite_monitor/main_v2.py
+++ kite_monitor/main_v2.py
@@ -213,6 +213,7 @@ def generate_signal_fm_df(df_ce: pd.DataFrame, df_pe: pd.DataFrame, details: dic
         df_ce.iloc[-2, atr_14_column_number] > df_ce.iloc[-2, moving_average_10_column_number],
         df_ce.iloc[-2, macd_7_20_1_column_number] > df_ce.iloc[-2, moving_average_20_triple_exponential_column_number]],
     ]
+    ]
     print(f"=====Put Side Check - Start - {str(datetime.datetime.now())}=========")
     print(tabulate(print_data, headers="firstrow", tablefmt="fancy_grid"))
     print(f"=====Put Side Check - End - {str(datetime.datetime.now())}===========")
